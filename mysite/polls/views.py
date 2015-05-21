5ae562b4f850f8c1455e21c9252e28db8e528a18
Christian McDonald <christian@cmcdonald.com>
2015-05-21 07:35:55 -0500

    cleaning up imports in polls views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render
 
 from .models import Question
 
